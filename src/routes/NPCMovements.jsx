0e02841249afbbad6d599c9c425ec1b236c1a5a6
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-05 23:31:46 +0200

    Back to safety

--- src/routes/NPCMovements.jsx
+++ src/routes/NPCMovements.jsx
@@ -22,26 +22,12 @@ const enemyTypes = [
     spriteWidth: 293,
     spriteHeight: 155,
     spriteFrames: 6,
-    bound: true,
-    move(x, y) {
-      return [
-        (x + Math.random() * 5 - 2.5),
-        (y + Math.random() * 5 - 2.5),
-      ];
-    },
   },
   {
     img: enemy2Img,
     spriteWidth: 266,
     spriteHeight: 188,
     spriteFrames: 6,
-    bound: false,
-    move(x, y) {
-      return [
-        (x),
-        (y),
-      ];
-    },
   },
   {
     img: enemy3Img,
@@ -69,8 +55,6 @@ class Enemy {
     this.spriteWidth = enemyType.spriteWidth;
     this.spriteHeight = enemyType.spriteHeight;
     this.spriteFrames = enemyType.spriteFrames;
-    this.bound = enemyType.bound;
-    this.move = enemyType.move;
   }
 
   draw(ctx, frameCount) {
@@ -86,39 +70,11 @@ class Enemy {
       this.spriteHeight / 2,
     );
   }
-
-  update(ctx) {
-    [this.x, this.y] = this.move(this.x, this.y);
-
-    if (this.bound) {
-      // Xbounds
-      if (this.x > ctx.canvas.width - this.spriteWidth / 2) {
-        this.x = ctx.canvas.width - this.spriteWidth / 2;
-      } else if (this.x < 0) { this.x = 0; }
-      // Ybounds
-      if (this.y > ctx.canvas.height - this.spriteHeight / 2) {
-        this.y = ctx.canvas.height - this.spriteHeight / 2;
-      } else if (this.y < 0) { this.y = 0; }
-    } else {
-      // xloop
-      if (this.x > ctx.canvas.width + 5) {
-        this.x = -this.spriteWidth / 2 - 5;
-      } else if (this.x < -this.spriteWidth - 5) {
-        this.x = ctx.canvas.width + 5;
-      }
-      // Yloop
-      if (this.y > ctx.canvas.height + 5) {
-        this.y = -this.spriteHeight / 2 - 5;
-      } else if (this.y < -this.spriteHeight - 5) {
-        this.y = ctx.canvas.height + 5;
-      }
-    }
-  }
 }
+
 const draw = (ctx, frameCount) => {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   enemyArr.forEach((enemy) => enemy.draw(ctx, frameCount));
-  enemyArr.forEach((enemy) => enemy.update(ctx));
 };
 
 function NPCMovements() {
@@ -128,13 +84,6 @@ function NPCMovements() {
     for (let i = 0; i < 5; i += 1) {
       enemyArr.push(new Enemy(
         enemyTypes[0],
-        Math.floor(Math.random() * (canvasRef.current.width - 200)),
-        Math.floor(Math.random() * (canvasRef.current.height - 200)),
-      ));
-    }
-    for (let i = 0; i < 5; i += 1) {
-      enemyArr.push(new Enemy(
-        enemyTypes[1],
         Math.floor(Math.random() * (canvasRef.current.width - 200)),
         Math.floor(Math.random() * (canvasRef.current.height - 200)),
       ));
